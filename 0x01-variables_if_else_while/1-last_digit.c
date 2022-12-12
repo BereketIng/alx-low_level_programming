193d60d33b87211511fa94450521cb7151c5ffde
BereketIng <bteketel@gmail.com>
2022-12-12 22:52:39 +0000

    C Programming alx-low_level_programming 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n, last_digit;
@@ -20,5 +20,6 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	else if (last_digit < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
+
 	return (0);
 }
