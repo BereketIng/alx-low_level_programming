68bf5d3c348f894cead347688999fffd83a15eba
BereketIng <bteketel@gmail.com>
2022-12-25 15:44:41 +0300

    100-main.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,26 +1,17 @@
 #include "main.h"
-#include "100-times_table.c"
-
 /**
- * main - check the code for alxs school students.
- *
- * Return: Always 0.
- */
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-   _putchar('\n');
-    print_times_table(15);
-    _putchar('\n');
-    print_times_table(16);
-   _putchar('\n');
-    print_times_table(0);
-    _putchar('\n');
-    return (0);
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
 }
